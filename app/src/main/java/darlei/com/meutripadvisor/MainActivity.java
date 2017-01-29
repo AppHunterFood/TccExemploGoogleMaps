8c21573f106d9149a55d5fd28718b9b8659766ea
Darlei de almeida damaceno <Darlei de almeida damaceno>
2017-01-29 21:23:39 -0200

    Initial commit

---
+++ app/src/main/java/darlei/com/meutripadvisor/MainActivity.java
@@ -0,0 +1,95 @@
+package darlei.com.meutripadvisor;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.Manifest;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> lugares;
+    static ArrayList<LatLng> localizacoes;
+    static ArrayAdapter arrayAdapter;
+
+    String[] permissoes = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PermissionUtils.validate(this, 0, permissoes);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        lugares= new ArrayList<>();
+        lugares.add("Adcione seu lugar favorito");
+
+        localizacoes = new ArrayList<>();
+        localizacoes.add(new LatLng (0,0));
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,lugares);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                intent.putExtra("Infolocalização",position);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int result : grantResults) {
+            if (result == PackageManager.PERMISSION_DENIED) {
+                // Alguma permissão foi negada, agora é com você :-)
+                alertAndFinish();
+                return;
+            }
+        }
+
+        // Se chegou aqui está OK :-)
+    }
+
+    private void alertAndFinish() {
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.app_name).setMessage("Para utilizar este aplicativo, você precisa aceitar as permissões.");
+            // Add the buttons
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    finish();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        }
+    }
+
+
+
+
+
+
+}
